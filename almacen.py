1bd7bd49cf4c3810396bfc84b1d4bd6fcc48dfe4
ElReyUnicornio <119967605+ElReyUnicornio@users.noreply.github.com>
2024-02-09 21:25:24 -0600

    feat(almacén): se añadieron metodos para almacén

--- almacen.py
+++ almacen.py
@@ -1,4 +1,7 @@
 from material import Equipo
+from Clase_encargado import Encargado
+from Clase_estudiante import Estudiante
+from datetime import date
 
 class almacen:
     inventario = []
@@ -10,5 +13,62 @@ class almacen:
         self.registros = []
         self.peticiones = []
 
-    def add_inventario(self, equipo: Equipo):
-        self.inventario.append(equipo)
+    def agregar_inventario(self, equipo: Equipo, cantidad: int = 1):
+        en_inventario = self.buscar_inventario(equipo.nombre)
+        if en_inventario != None:
+            en_inventario['cantidad'] += cantidad
+            return
+        
+        self.inventario.append({
+            'equipo': equipo,
+            'cantidad': cantidad
+        })
+        
+    #metodos para manejar el inventario
+    def remover_inventario(self, equipo: Equipo, cantidad: int = 1):
+        self.agregar_inventario(equipo, -cantidad)
+        
+    def buscar_inventario(self, nombre: str):
+        for equipo in self.inventario:
+            if equipo['equipo'].nombre == nombre:
+                return equipo
+        return None
+    
+    #metodos para manejar los registros
+    def agregar_registro(self, peticionID: int, encargado: Encargado):
+        self.peticiones[peticionID]['aprovada'] = True
+        self.registros.append({
+            'petición': self.peticiones[peticionID],
+            'encargado': encargado,
+            'fecha': date.now()
+        })
+        
+    def buscar_registro(self, nombre: str):
+        registros = []
+        for id, registro in enumerate(self.registros):
+            if registro['persona'].nombre == nombre or registro['persona'].matricula == nombre:
+                registros.append((id, registro))
+        return registros
+    
+    def obtener_registro(self, id: int):
+        return self.registros[id]
+    
+    #metodos para manejar las peticiones
+    def agregar_peticion(self, equipo: Equipo, cantidad: int, persona: Estudiante):
+        self.peticiones.append({
+            'equipo': equipo,
+            'cantidad': cantidad,
+            'persona': persona,
+            'aprovada': False,
+            'fecha': date.now()
+        })
+        
+    def buscar_peticion(self, nombre: str):
+        peticiones = []
+        for id, peticion in enumerate(self.peticiones):
+            if (peticion['persona'].nombre == nombre or peticion['persona'].matricula == nombre) and not peticion['aprovada']:
+                peticiones.append((id, peticion))
+        return peticiones
+    
+    def obtener_peticion(self, id: int):
+        return self.peticiones[id]
--- main.py
+++
@@ -1,6 +0,0 @@
-print("Menu ")
-respuesta=0
-print("preciona 1 o 0")
-respuesta=input("Entrar al sistema ")
-if respuesta==1:
-    5 
